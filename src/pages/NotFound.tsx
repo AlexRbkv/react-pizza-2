330e809b1a340b144e9436f00c6ebd47c6f7f950
AlxRbkv <wsro2012@mail.ru>
2022-07-11 14:29:37 +0400

    deploy

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -6,7 +6,7 @@ import styles from '../components/NotFoundBlock/NotFound.module.scss';
 const NotFound: React.FC = () => {
   return (
     <div className={styles.root}>
-      <h1>aa</h1>
+      <h1>О-оу</h1>
       <NotFoundBlock />
     </div>
   );
